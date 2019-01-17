2302b863990f8cf6c2ebe3a616134f2f2146f350
Olivia Kesler <oakesler@protonmail.ch>
2019-01-17 23:39:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -81,7 +81,9 @@ function removeFromCart(item) {
       cart.splice(i, 1)
       return cart
     }
-    else return "That item is not in your cart."
+    else {
+      return "That item is not in your cart."
+    }
   }
 }
 
