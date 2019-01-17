cf4fb81c2a86adff6c2c6bc9334f46560718b184
Olivia Kesler <oakesler@protonmail.ch>
2019-01-17 22:54:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -81,6 +81,7 @@ function removeFromCart(item) {
   for(var i = 0; i < carty.length; i++) {
     if (carty[i].itemName === itemy) {
       carty.prototype.splice(i, 1)
+      return carty
     }
     else {
       return "That item is not in your cart."
