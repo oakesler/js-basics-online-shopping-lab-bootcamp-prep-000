1152ecc114f60ed96b52bf39003f99924290f47b
Olivia Kesler <oakesler@protonmail.ch>
2019-01-18 00:00:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ function placeOrder(cardNumber) {
   var x = 0 + cardNumber
   var z = cart.length
   
-  if (x < 0) {
+  if (x === undefined) {
     return "Sorry, we don't have a credit card on file for you."
   }
     
